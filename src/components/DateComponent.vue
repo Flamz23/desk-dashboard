0a36e23deee8a2caf9174fa77957d71971ba7288
Dami <59375070+Flamz23@users.noreply.github.com>
2022-12-25 18:40:52 -0500

    added date component

---
+++ src/components/DateComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>
+            {{ msg }}
+        </h1>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+}
+</style>
