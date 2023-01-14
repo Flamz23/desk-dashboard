aa3506baff711694fefb0935b91ca58144f03478
Dami <59375070+Flamz23@users.noreply.github.com>
2023-01-13 21:16:11 -0500

    Adjusted padding and margins for DrawOverComponent

--- src/components/DrawOverComponent.vue
+++ src/components/DrawOverComponent.vue
@@ -1,11 +1,12 @@
 <template>
     <nav class="drawover-nav">
-        <button class="nav-buttons">
-            <IconFullscreen />
-        </button>
-        <button class="nav-buttons">
-            <IconSettings />
-        </button>
+        <ul class="setting-list">
+            <li>
+                <div class="icon-wrapper">
+                    <IconSettings />
+                </div>                
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -16,15 +17,42 @@ import IconFullscreen from './icons/IconFullscreen.vue';
 
 <style>
 .drawover-nav {
-    background-color: #212529;
+    display: flex;
+
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    margin: 0.5rem;
+
+    width: 40%;
+    min-width: 20rem;
+    height: auto;
+    padding: 0.5rem;
+    
+
+    border-radius: 0.8rem;
+
+    background-color: #343A40;
 }
-.nav-buttons {
+
+.setting-list {
+    width: 100%;
+    padding: 0;
+}
+
+.setting-list > li {
+    margin: 0.4rem 0 0.4rem 0;
+    border-radius: 0.8rem;
+    background-color: #495057;
+}
+
+.icon-wrapper {
     border: none;
     border-color: #6C757D;
     border-radius: 50%;
     background-color: #212529;
 }
 svg {
-    fill: #6C757D;
+    fill: #CED4DA;
 }
 </style>
